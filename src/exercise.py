10cc9569505f4085ff11902e5ae3f9b3a5dccfd7
Jaime Santos <50669048+JaimePSantos@users.noreply.github.com>
2020-10-21 12:38:22 +0100

    ex23

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,9 @@
 def main():
     #write your code below this line
+    number = int(input())
+    if(number<0):
+        number = number*(-1)
+    print(number)
 
 if __name__ == '__main__':
     main()
